243553bcf5b3254e4fa42f7475ca411202203028
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-08-20 12:02:12 +0600

    ticket value connect

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-function ticketChangeHandler(isIncrease){
-    const inputValue = document.getElementById("first-class").value;
-    const inputValueNumber = parseInt(inputValue);
-    let newCount = inputValueNumber;
+function ticketChangeHandler(ticketQuality, isIncrease){
+    const inputValueNumber = getInputNumber(ticketQuality)
+    let newTicketCount = inputValueNumber;
     if(isIncrease == true){
-        newCount = inputValueNumber + 1;
+        newTicketCount = inputValueNumber + 1;
     }
     if(isIncrease == false && inputValueNumber > 0){
-        newCount = inputValueNumber - 1;
+        newTicketCount = inputValueNumber - 1;
     }
-    document.getElementById("first-class").value = newCount;
+    document.getElementById(ticketQuality).value = newTicketCount;
+    calculatorAmount();
+}
+function calculatorAmount() {
+    const firstClassTicketsNumber = getInputNumber('first-class');
+    const economyClassTicketsNumber = getInputNumber('economy-class');
 
-    const subtotalAmount = newCount * 150;
+    const firstClassTicketsCost = firstClassTicketsNumber * 150;
+    const economyClassTicketsCost = economyClassTicketsNumber * 100;
+
+    const subtotalAmount = firstClassTicketsCost + economyClassTicketsCost;
     document.getElementById("subtotal-amount").innerText = subtotalAmount;
     const vatAmount = subtotalAmount * 0.1;
     document.getElementById("vat-amount").innerText = vatAmount;
     const totalAmount = subtotalAmount + vatAmount;
     document.getElementById("total-amount").innerText = totalAmount;
+};
+
+function getInputNumber(ticketQuality){
+    const Tickets = document.getElementById(ticketQuality).value;
+    const TicketsNumber = parseInt(Tickets);
+    return TicketsNumber;
 }
